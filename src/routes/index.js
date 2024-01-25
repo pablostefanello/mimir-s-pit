a7127229c5f9ad02ee194499005da79014a86ad3
Tobias Cadoná Marion <71554245+tobiasMarion@users.noreply.github.com>
2024-01-25 00:13:27 -0300

    aaaaaaaa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-
 router.get('/', (req, res) => {
     res.render('home')
 })
 
+router.get('/test', (req, res) => {
+    res.render('testForm')
+})
 
 module.exports = router
